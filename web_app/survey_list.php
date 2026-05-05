63e626d6f35c37df849974e1c1378dfaece4a184
kaito1105 <kai20011105@gmail.com>
2026-05-05 02:24:07 -0500

    added survey list file

---
+++ web_app/survey_list.php
@@ -0,0 +1,61 @@
+<?
+require 'init.php';
+
+$result = lib::db_query(
+  "SELECT * FROM " . FORM_TABLE . " ORDER BY recipe_name ASC"
+);
+$rows = $result->fetchAll();
+$num_rows = count($rows);
+
+$security = false;
+require 'ssi_top.php';
+?>
+
+<a href="affiliate_survey.php">Go to the data form</a>
+<br><br>
+
+<? if ($num_rows == 0) { ?>
+  <b>No records were found in the database.</b>
+<? } else { ?>
+  <? if ($get_post['complete'] ?? '') { ?>
+    Thank you for submitting the data form.
+  <? } ?>
+
+  <b>Listing of Database Records:</b>
+
+  <table width="" border="1" cellspacing="0" cellpadding="5">
+    <tr valign="top">
+      <th>Recipe Name</th>
+      <th>Email</th>
+      <th>Preparation Date</th>
+      <th>Total Cooking Time<br>(minutes)</th>
+      <th>Difficulty Level<br>(1 = Easy, 10 = Hard)</th>
+      <th>Meal Time</th>
+      <th>Dish Type</th>
+      <th>Main Cooking Method</th>
+      <th>Proteins Used</th>
+      <th>Additional Ingredients</th>
+      <th>Cooking Instructions</th>
+    </tr>
+    <? foreach ($rows as $row) { ?>
+      <tr valign="top">
+        <td><?= $row['recipe_name'] ?></td>
+        <td><?= $row['email'] ?></td>
+        <td><?= $row['preparation_date'] ?></td>
+        <td><?= $row['total_cooking_time'] ?></td>
+        <td><?= $row['difficulty_level'] ?></td>
+        <td><?= $row['meal_time'] ?></td>
+        <td><?= $row['dish_type'] ?></td>
+        <td><?= $row['main_cooking_method'] ?></td>
+        <td><?= $row['proteins_used'] ?></td>
+        <td><?= $row['additional_ingredients'] ?></td>
+        <td><?= $row['cooking_instructions'] ?></td>
+      </tr>
+    <? } ?>
+  </table>
+
+<? } ?>
+
+<?
+require 'ssi_bottom.php';
+?>
