54f7f4aa2152416fe6f6dfa9310869eb4dc02e73
kaito1105 <kai20011105@gmail.com>
2026-05-05 00:16:33 -0500

    created affiliate survey file

---
+++ web_app/affiliate_survey.php
@@ -0,0 +1,9 @@
+<?
+require 'init.php';
+
+define('SURVEY_API_URL',   'https://csci.lakeforest.edu/~miyamotok/csci488/web_app/api_survey.php');
+define('SURVEY_API_TOKEN', '8f9d8313cbc55922b6ba34194ab5599d64ed12f3161813193c63ff724cee9f13');
+
+
+
+?>
